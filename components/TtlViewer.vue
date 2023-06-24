183df0e0ea52cd99486357f25d43241a85585d6e
Tim Standen <timmstanden@gmail.com>
2023-06-24 00:32:09 -0700

    Add two js display to front page

---
+++ components/TtlViewer.vue
@@ -0,0 +1,28 @@
+<script setup>
+  import Two from "two.js";
+  const stage = ref(null);
+  
+  function initTwo() {
+    const two = new Two({
+      fullscreen:true
+    }).appendTo(stage.value);
+
+    const x = two.width * 0.5;
+    const y = two.height * 0.5;
+    const width = 100;
+    const height = 100;
+    const rect = two.makeRectangle(x, y, width, height);
+
+    rect.fill = 'rgb(0, 200, 255)';
+    two.update();
+  }
+
+  onMounted(() => {
+    initTwo();
+  })
+
+</script>
+
+<template>
+  <div ref='stage' />
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,12 @@
+<script setup>
+  const counter = useState('counter')
+</script>
+
 <template>
   <div>
     <h1>Welcome to my homepage</h1>
     <NuxtLink to="/about">About Page</NuxtLink>
+    <p>Counter: {{ counter }}</p>
+    <TtlViewer/>
   </div>
 </template>
